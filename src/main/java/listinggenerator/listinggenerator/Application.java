3b7639319b21c3a8312b8446871320f97b497e24
OlgaRhythm <y.o.a@yandex.ru>
2024-01-13 15:37:44 +0300

    Изменене внешнего вида окна

--- src/main/java/listinggenerator/listinggenerator/Application.java
+++ src/main/java/listinggenerator/listinggenerator/Application.java
@@ -11,6 +11,7 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("menu-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 750, 400);
+        stage.setResizable(false);
         MenuController controller = fxmlLoader.getController();
         controller.start(stage, scene);
     }
