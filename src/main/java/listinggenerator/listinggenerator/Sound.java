4315d739e100fc22a0094f8639cd9956691f0c84
OlgaRhythm <y.o.a@yandex.ru>
2024-01-19 18:45:46 +0300

    Воспроизведение музыки при нажатии на кнопку звука

---
+++ src/main/java/listinggenerator/listinggenerator/Sound.java
@@ -0,0 +1,48 @@
+package listinggenerator.listinggenerator;
+
+import javax.sound.sampled.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+public class Sound {
+    private Clip clip;
+
+    Sound(String fileName) {
+        loadSound(fileName);
+    }
+
+    public void loadSound(String fileName){
+        try {
+            InputStream inputStream = Sound.class.getResourceAsStream("/music/" + fileName);
+
+            if (inputStream == null) {
+                throw new IllegalArgumentException("Resource not found: " + inputStream);
+            }
+
+            // Чтение данных из InputStream в байтовый массив
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                byteArrayOutputStream.write(buffer, 0, bytesRead);
+            }
+
+            // Преобразование байтового массива в ByteArrayInputStream
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+
+
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(byteArrayInputStream);
+
+            clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+
+        } catch (Exception ex) {
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
+        }
+    }
+    public void playSound() {
+        clip.start();
+    }
+}
